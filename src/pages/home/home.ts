50ad64813ff88451c846528d4125d59f4d1656d2
monashperera <monashperera@gmail.com>
2017-12-04 15:42:56 +0530

    get

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TestmessProvider } from '../../providers/testmess/testmess';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[TestmessProvider]
 })
 export class HomePage {
+  public resData;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public testmessProvider:TestmessProvider) {
+    testmessProvider.load()
+    .then(data => {
+      console.log(data);
+      this.resData = data;
+
+    })
 
   }
 
--- src/providers/testmess/testmess.ts
+++ src/providers/testmess/testmess.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the TestmessProvider provider.
@@ -9,9 +10,33 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class TestmessProvider {
+  data: any;
 
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
     console.log('Hello TestmessProvider Provider');
+  }
+  load() {
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+    let opt: RequestOptions;
+    let myHeaders: Headers = new Headers;
+
+    myHeaders.set('Accept', 'application/json; charset=utf-8');
+    myHeaders.append('Content-type','application/json; charset=utf-8');
+    opt = new RequestOptions({
+      headers : myHeaders
+
+    })
+    return new Promise(resolve =>{
+      this.http.get('http://localhost/monash/jp.php',opt)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.data = data;
+        resolve(this.data);
+      });
+    });
   }
 
 }
